ef37e588f1a94c731393bb3debfb8bf517b02922
rublen <rublen77@gmail.com>
2017-02-05 22:13:57 +0000

    + comments

--- braces_vs_doend.rb
+++ braces_vs_doend.rb
@@ -2,13 +2,12 @@ arr = [11,88,33,44,66,99,77]
 p arr.find_all { |n| n > 66 }
 
 puts '*****'
-
+# "If no block is given, an Enumerator is returned instead." - from ruby-doc.org
 p arr.find_all do |n|
   n > 66
 end
 
 puts '*****'
-
 p (arr.find_all do |n| 
   n > 66
 end)
@@ -19,7 +18,7 @@ arr_sym = %i(a b c p k )
 p arr_sym.find_all { |s| ('a'..'f') === s.to_s }
 
 puts '*****'
-
+# "If no block is given, an Enumerator is returned instead."
 p arr_sym.find_all do |s| 
   ('a'..'f') === s.to_s
 end
@@ -29,7 +28,9 @@ puts "---------------------------------"
 p arr.any? { |n| n > 66 }
 
 puts '*****'
-
+# "If the block is not given, Ruby adds an implicit block of { |obj| obj } 
+# that will cause any? to return true if at least one of the collection members 
+# is not false or nil."
 p arr.any? do |n|
   n > 66
 end
@@ -39,7 +40,8 @@ puts "---------------------------------"
 p arr.all? { |n| n > 66 }
 
 puts '*****'
-
+# "If the block is not given, Ruby adds an implicit block of { |obj| obj } which will
+# cause all? to return true when none of the collection members are false or nil."
 p arr.all? do |n|
   n > 66
 end
