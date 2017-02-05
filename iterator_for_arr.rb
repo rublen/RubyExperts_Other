5d070031e87e8bd24d1b890a5bbc6d20ff6e74c1
rublen <rublen77@gmail.com>
2017-02-05 23:40:13 +0000

    after robocop

--- iterator_for_arr.rb
+++ iterator_for_arr.rb
@@ -1,11 +1,11 @@
 # New method for arrays
 class Array
-  def map_with_index(&bl)
-    self.map do |val|
-      yield(val, self.index(val))
+  def map_with_index
+    map do |val|
+      yield(val, index(val))
     end
   end
 end
 
-[1, 2, 3, 4].map_with_index { |v, i| p "a[#{i}]=#{v}"}
-p [1, 2, 3, 4].map_with_index { |v, i| if i.odd? then v * 2 else v end }
+[1, 2, 3, 4].map_with_index { |v, i| puts "a[#{i}]=#{v}" }
+p [1, 2, 3, 4].map_with_index { |v, i| i.odd? ? v * 2 : v }
